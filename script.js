eb34980102b6289c2e4688d3c589e5d83b110561
Clarke <thatclarkemacarthur@gmail.com>
2020-11-14 14:25:58 -0500

    Figured out (with help from Ermek!) how to limit number of appends for each die!:sparkles::dragon::sparkles:

--- script.js
+++ script.js
@@ -1,55 +1,94 @@
-//make sure to have an empty() on the clear rolls button
+
 //check if document ready
-$('.roll').click(function(){
-    //on click/submit(might make this a form), a random number within the min/max number for that die will be selected probably using Math.floor and Math.random
-    
-    
-    const d4 = Math.floor(Math.random() * 4) + 1;
-    const d6 = Math.floor(Math.random() * 6) + 1;
-    const d8 = Math.floor(Math.random() * 8) + 1;
-    const d10 = Math.floor(Math.random() * 10) + 1;
-    const d12 = Math.floor(Math.random() * 12) + 1;
-    const d20 = Math.floor(Math.random() * 20) + 1;
-    
-    //display results on li's under their respective ul's
-    switch (this.id) {
-        case 'd4':
-            $('.d4_rolls').append(`<li><p>${d4}</p></li>`);
-            break;
-            
-        case 'd6':
-            $('.d6_rolls').append(`<li><p>${d6}</p></li>`);
-            break;
-        case 'd8':
-            $('.d8_rolls').append(`<li><p>${d8}</p></li>`);
-            break;
-        case 'd10':
-            $('.d10_rolls').append(`<li><p>${d10}</p></li>`);
-            break;
-        case 'd12':
-            $('.d12_rolls').append(`<li><p>${d12}</p></li>`);
-            break;
-        case 'd20':
-            $('.d20_rolls').append(`<li><p>${d20}</p></li>`);
-            break;
-    }
-    // $('#d20_rolls').append(`<li><p>${d20}</p></li>`);
-    // $('#d12_rolls').append(`<li><p>${d12}</p></li>`);
-    // $('#d10_rolls').append(`<li><p>${d10}</p></li>`);
-    // $('#d8_rolls').append(`<li><p>${d8}</p></li>`);
-    // $('#d6_rolls').append(`<li><p>${d6}</p></li>`);
-    // $('#d4_rolls').append(`<li><p>${d4}</p></li>`);
-})
-
-//limit results to ten or twenty li's
-
-//make sure to have an empty() on the clear rolls button
-$('#clear').on('click', function(){
-    $('li').empty();
-});
-
-
-
 $(function() {
-//check if document ready
+
+    //Declaring variables for each die to count number of times rolled and displayed:
+    let d4Counter = 0;
+    let d6Counter = 0;
+    let d8Counter = 0;
+    let d10Counter = 0;
+    let d12Counter = 0;
+    let d20Counter = 0;
+    
+    //first event listener: rolls dice and displays the results to the page
+    $('.roll').on('click', function(){
+        
+        //on click a random number within the min/max number for that die will be selected using Math.floor and Math.random
+            
+            const d4 = Math.floor(Math.random() * 4) + 1;
+            const d6 = Math.floor(Math.random() * 6) + 1;
+            const d8 = Math.floor(Math.random() * 8) + 1;
+            const d10 = Math.floor(Math.random() * 10) + 1;
+            const d12 = Math.floor(Math.random() * 12) + 1;
+            const d20 = Math.floor(Math.random() * 20) + 1;
+            
+            //display results on li's under their respective ul's
+            //limit results to ten or twenty li's
+            switch (this.id) {
+                case 'd4':
+                    if (d4Counter < 10) {
+                        d4Counter++
+                        $('.d4_rolls').append(`<li><p>${d4}</p></li>`);
+                    } else {
+                        break;
+                    }
+                    break;
+        
+                    case 'd6':
+                    if (d6Counter < 10) {
+                        d6Counter++
+                        $('.d6_rolls').append(`<li><p>${d6}</p></li>`);
+                    } else {
+                        break;
+                    }
+                        break;
+
+                    case 'd8':
+                    if (d8Counter < 10) {
+                        d8Counter++
+                        $('.d8_rolls').append(`<li><p>${d8}</p></li>`);
+                    } else {
+                        break;
+                    }
+                        break;
+
+                    case 'd10':
+                    if (d10Counter < 10) {
+                        d10Counter++
+                        $('.d10_rolls').append(`<li><p>${d10}</p></li>`);
+                    } else {
+                        break;
+                    }
+                        break;
+
+                    case 'd12':
+                    if (d12Counter < 10) {
+                        d12Counter++
+                        $('.d12_rolls').append(`<li><p>${d12}</p></li>`);
+                    } else {
+                        break;
+                    }
+                        break;
+
+                    case 'd20':
+                    if (d20Counter < 10) {
+                        d20Counter++
+                        $('.d20_rolls').append(`<li><p>${d20}</p></li>`);
+                    } else {
+                        break;
+                    }
+                        break;
+                }
+        })
+        
+        //second event listener: clears results and resets the roll counters to 0
+        $('#clear').on('click', function(){
+            $('li').empty();
+            d4Counter = 0;
+            d6Counter = 0;
+            d8Counter = 0;
+            d10Counter = 0;
+            d12Counter = 0;
+            d20Counter = 0;
+        });
 })
